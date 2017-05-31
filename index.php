70f4d6e0a8a5aaeb3cbd6640649379a661ffba95
Danila Chenchik <chenchik@email.unc.edu>
2017-05-31 12:16:41 -0400

    added lobster

--- index.php
+++ index.php
@@ -1,5 +1,61 @@
 <?php
 
 echo "super simple hello world using openshift";
+echo "            `://///+:               oo++++/`                
+         `/++:-::/+yy+              o-o/:-:oo/              
+       `+o-.-::///d+-+/             s-:o/::::/s/            
+      -s-.--:://:+/--/s             -o-://::::-/s-          
+     :s..-:///:-+--::s:              +/-://::/+::oo         
+    -s..-:::/:-/:-:+/y                s:-://-:/+/:+s        
+   `y...:::::----:-:s-                `y:-:::-:/+/:/s       
+   +:..-/:-:/:.---:+o                  `y-.-::.:/+/:+o      
+   s..-::--::-//::oy                    .y::::-::/+/-o:     
+   s--::--:::::-::d-                     `y/--:/::/+/:y     
+  .o:::--://::///oo        :s+`           ++----:::://o:    
+  :+::--:://::::oo         -MM+           `++-.-:::///+y    
+  /+::-://///-/s/           ydh`            /s---::///+y:   
+  /o/::////::/y:            -dys             -s---:////oh   
+  :s:::/::/s+:.          `--`yoh.`--`         .+o::///++y-  
+  `y//+:--/y-          `+o++sh/oso++s/          .y/.-:/+os  
+   -h/o//:::/o+:`     -s+yd+:/+//-:dy+s`     .:/+/--:/+o/s: 
+    h-:+o+/::::/+o+` /s+soy/::/:/-/sys+s` -/+/:-..:/+/oy--y 
+    o+/osso+/:::--:sooodhh+:://://+oyoy+s++..-.--/++++o+-/y 
+    `ss//ss+o+/:-.-:/ss-d+/::://+//++dsy+/.`.-:////+shs:/y: 
+      so/sdys+:----:-:+s///:/:////:/+o+--:-..--//os+ss/os-  
+      .do/y.+yy+:::-.--:/+///:://///+/-..-:---/oo:`oo/y+`   
+       /dooo:-+h++:-.-:/++///::///+/++:----//s+.  :s/sy     
+///:-..`:yo/++ooh+o+/-/+//o+//:://+++++///+ssh.`-:s/sh.     
+ysyhyys++osys+/:+++/oy/sooso+//://++++oso.`.yos+++/ss.`..--.
+`..-:/+syoossh+o+/:////osyyso/::/++++o++s++//://oyhs+ossssyy
+  :oooo+sdy++os-.:+++o++/oys+/:://+++++/::///+oyysoosso+//-`
+  +ssssyhss+oooso-``ysooo+oo+/::/+oo++++y/-..-s+//ys:`      
+     `-yhsshyo+ysoo+y+ooosoo+/::+ooo++/+d`.:ooo+++sy-       
+     .yhh++odmyoydyo/+oooooo+/-:/+o+++++o++osho+ososh+.     
+  ./+yyhysyyyyydyo+o++os++o++:--/+o+++o+o+osh++h+.:yosyo.   
+  /dhy/-` `-/sssso+/-+sso+o+/--:/+++++o+h+o+/ossy: -ysysh`  
+  -mh.        `-:/oooossoo++/:::/+o++oo++/:os++yoh  .ymsy.  
+ -my`               ./do++++/::////+++sdy+s+.  osh.  .+hy`  
+-my`                  ohs+ooo++++oosssyoo/.    `hso    -.   
+my`                   :yoo+/:--:/ooossd`        s+h         
+h`                    .hooooo:::/++oood`        soy         
+.                     `msysso+///++sood:         ``         
+                       myso+++::/++ossd.                    
+                       hyoooo+:-::/+osd`                    
+                       msoo+oo/::/+oosho                    
+                       yyso/oo/://+oosd.                    
+                       oyoooss+///++osd-                    
+                       syooooo/:/++oosh/                    
+                       .mooooo/-::/+osd                     
+                       oysooso/::/+oosh/                    
+                       :dsooo+/:::/++sh-                    
+                        dso++o+:://++yh                     
+                      `+hoo+++/::/++ood:`                   
+                  `+ssyyo+++++/:://://ossso+-               
+                .ohyssso+//////-:::::://+++oys-             
+               `hsooo++oo//++o+:////:////+++++h-            
+               :hoo+++osoo/ooo+/+++/////+/+++/y:            
+                yhsooooosso+so+++++://///////y+             
+                 -+oooooooo+ossssss+///+++++/.              
+";
 
 ?>
